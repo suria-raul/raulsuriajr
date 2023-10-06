ee54bee599c8113b6f30fcd2788511a37b35d9a9
vava_raul <raul@vavasoftware.com>
2023-10-06 14:35:26 +0800

    Projects.vue Created

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import data from './data'
 import Profile from "@/components/Profile.vue"
 import WorkExperience from "@/components/WorkExperience.vue"
+import Projects from "@/components/Projects.vue"
 
 export default {
   components: {
+    Projects,
     Profile,
     WorkExperience
   },
@@ -19,6 +21,7 @@ export default {
 <template>
   <Profile />
   <WorkExperience :work-experience="data.workExperience"/>
+  <Projects :projects="data.projects" />
 </template>
 
 <style scoped>
--- src/data.js
+++ src/data.js
@@ -79,101 +79,107 @@ const data = {
         }
     },
     projects: {
-        charlton_media: {
-            name: 'Charlton Media',
-            technology: 'Drupal 9',
-            image: 'images/charlton_media.png',
-            link: 'https://charltonmedia.com/',
-        },
-        elbien_hotel: {
-            name: 'El bien hotel tagaytay',
-            technology: 'Drupal 9',
-            image: 'images/elbien_hotel_tagaytay.png',
-            link: 'https://elbienhotel.com/',
-        },
-        alstra: {
-            name: 'Alstra Contact Page',
-            technology: 'Drupal 8',
-            image: 'images/alstra_form.png',
-            link: 'https://ccs.alstra.ph/',
-        },
-        wellness_home_massage: {
-            name: 'Royal Wellness',
-            technology: 'Drupal 9',
-            image: 'images/royal_wellness.png',
-            link: 'https://wellnesshomemassage.com/',
-        },
-        bonakid: {
-            name: 'Bonakid',
-            technology: 'Drupal 8',
-            image: 'images/bonakid.png',
-            link: 'https://batangmaylaban.com.ph',
-        },
-        dryBrush: {
-            name: 'drybrush',
-            technology: 'Laravel',
-            image: 'images/dryBrush.png',
-            link: 'https://drybrush.com/',
-        },
-        mom_baby_hub: {
-            name: 'Mom and Baby Hub',
-            technology: 'Laravel',
-            image: 'images/momandbaby.png',
-            link: 'https://momandbabyhub.com/',
+        title: 'Projects',
+        projects: {
+            charlton_media: {
+                name: 'Charlton Media',
+                technology: 'Drupal 9',
+                image: 'images/charlton_media.png',
+                link: 'https://charltonmedia.com/',
+            },
+            elbien_hotel: {
+                name: 'El bien hotel tagaytay',
+                technology: 'Drupal 9',
+                image: 'images/elbien_hotel_tagaytay.png',
+                link: 'https://elbienhotel.com/',
+            },
+            alstra: {
+                name: 'Alstra Contact Page',
+                technology: 'Drupal 8',
+                image: 'images/alstra_form.png',
+                link: 'https://ccs.alstra.ph/',
+            },
+            wellness_home_massage: {
+                name: 'Royal Wellness',
+                technology: 'Drupal 9',
+                image: 'images/royal_wellness.png',
+                link: 'https://wellnesshomemassage.com/',
+            },
+            bonakid: {
+                name: 'Bonakid',
+                technology: 'Drupal 8',
+                image: 'images/bonakid.png',
+                link: 'https://batangmaylaban.com.ph',
+            },
+            dryBrush: {
+                name: 'drybrush',
+                technology: 'Laravel',
+                image: 'images/dryBrush.png',
+                link: 'https://drybrush.com/',
+            },
+            mom_baby_hub: {
+                name: 'Mom and Baby Hub',
+                technology: 'Laravel',
+                image: 'images/momandbaby.png',
+                link: 'https://momandbabyhub.com/',
+            }
         }
     },
     skills: {
-        drupal: {
-            name: 'Drupal',
-            icon: 'images/icons/drupal.png',
-        },
-        laravel: {
-            name: 'Laravel',
-            icon: 'images/icons/laravel.png',
-        },
-        php: {
-            name: 'PHP',
-            icon: 'images/icons/php.png',
-        },
-        mysql: {
-            name: 'MySQL',
-            icon: 'images/icons/mysql.png',
-        },
-        git: {
-            name: 'Git',
-            icon: 'images/icons/git.png',
-        },
-        javascript: {
-            name: 'JavaScript',
-            icon: 'images/icons/javaScript.png',
-        },
-        // vue: {
-        //     name: 'Vue',
-        //     icon: 'images/icons/vue.png',
-        // },
-        jquery: {
-            name: 'JQuery',
-            icon: 'images/icons/jquery.png',
-        },
-        tailwindcss: {
-            name: 'Tailwind Css',
-            icon: 'images/icons/tailwindcss.png',
-        },
-        bootstrap: {
-            name: 'Bootstrap',
-            icon: 'images/icons/bootstrap.png',
-        },
-        sass: {
-            name: 'Sass',
-            icon: 'images/icons/sass.png'
-        },
-        css3: {
-            name: 'CSS',
-            icon: 'images/icons/css3.png',
-        },
-        html: {
-            name: 'HTML',
-            icon: 'images/icons/html5.png',
+        title: 'Skills',
+        skills: {
+            drupal: {
+                name: 'Drupal',
+                icon: 'images/icons/drupal.png',
+            },
+            laravel: {
+                name: 'Laravel',
+                icon: 'images/icons/laravel.png',
+            },
+            php: {
+                name: 'PHP',
+                icon: 'images/icons/php.png',
+            },
+            mysql: {
+                name: 'MySQL',
+                icon: 'images/icons/mysql.png',
+            },
+            git: {
+                name: 'Git',
+                icon: 'images/icons/git.png',
+            },
+            javascript: {
+                name: 'JavaScript',
+                icon: 'images/icons/javaScript.png',
+            },
+            vue: {
+                name: 'Vue',
+                icon: 'images/icons/vue.png',
+            },
+            jquery: {
+                name: 'JQuery',
+                icon: 'images/icons/jquery.png',
+            },
+            tailwindcss: {
+                name: 'Tailwind Css',
+                icon: 'images/icons/tailwindcss.png',
+            },
+            bootstrap: {
+                name: 'Bootstrap',
+                icon: 'images/icons/bootstrap.png',
+            },
+            sass: {
+                name: 'Sass',
+                icon: 'images/icons/sass.png'
+            },
+            css3: {
+                name: 'CSS',
+                icon: 'images/icons/css3.png',
+            },
+            html: {
+                name: 'HTML',
+                icon: 'images/icons/html5.png',
+            }
         }
     },
 }
