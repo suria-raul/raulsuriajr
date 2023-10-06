0f056c991fb6184ac2e580fa6bf1f11d234b423a
suria-raul <stadnitzki.nero.nero@gmail.com>
2023-10-06 21:47:49 +0800

    reverted changes to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:suria-raul/raulsuriajr.git master
-
+git push -f git@github.com:suria-raul/raulsuriajr.git master:master
+  
 cd -
