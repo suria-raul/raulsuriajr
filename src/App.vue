f503239bb41032d3d75a2b250584693e7048587f
vava_raul <raul@vavasoftware.com>
2023-10-06 15:47:26 +0800

    Skills.vue Created

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@ import data from './data'
 import Profile from "@/components/Profile.vue"
 import WorkExperience from "@/components/WorkExperience.vue"
 import Projects from "@/components/Projects.vue"
+import Skills from "@/components/Skills.vue"
 
 export default {
   components: {
+    Skills,
     Projects,
     Profile,
     WorkExperience
@@ -22,6 +24,7 @@ export default {
   <Profile />
   <WorkExperience :work-experience="data.workExperience"/>
   <Projects :projects="data.projects" />
+  <Skills :skills="data.skills" />
 </template>
 
 <style scoped>
