9cc41b70e50ed57dc688d2a9debe6fbd923502bb
suria-raul <stadnitzki.nero.nero@gmail.com>
2024-01-15 00:34:15 +0800

    updated deploy.sh file commands Added echos to make it more comprehensive

--- deploy.sh
+++ deploy.sh
@@ -2,34 +2,52 @@
 
 set -e
 
+echo "Npm Version"
 npm --version
 
+echo "Running npm install..."
+npm install
+
+echo "Building..."
 npm run build
 
+echo "Creating submodule folder..."
+mkdir submodule
+
+echo "Entering submodule folder"
 cd submodule
 
+echo "Adding submodule repository"
 git submodule add -f git@github.com:suria-raul/raulsuriajr.git
 
+echo "Entering dist folder inside submodule folder"
 cd ../dist
 
+echo "Copying all files to raulsuriajr folder"
 cp * ../submodule/raulsuriajr -r
 
+echo "Change directory to assets folder"
 cd ../submodule/raulsuriajr/assets
 
+echo "Renaming Css Index"
 mv index-*.css index.css
 
+echo "Renaming Js Index"
 mv index-*.js index.js
 
+echo "Copying profile.jpg"
 mv profile-*.jpg profile.jpg
 
-cd -
+echo "Going back to the root directory"
+cd - && cd ../ && pwd
 
-cd ../
-
+echo "Removing changes to .gitmodules file"
 git restore --staged .gitmodules
 
+echo "Deleting .gitmodules file"
 rm .gitmodules
 
+echo ""
 git restore --staged submodule
 
 echo "Build done!"
